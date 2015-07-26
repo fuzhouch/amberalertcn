5e316e5c3d780440fe74d233c3b2f065a2dd1838
unknown <v-xuesli@RTCFUZHOUCHM1.redmond.corp.microsoft.com>
2015-07-26 13:01:29 +0800

    Add http client and change encodings.

--- client/AacnClient/app/src/main/java/com/hackthon/amberalertcn/client/MainActivity.java
+++ client/AacnClient/app/src/main/java/com/hackthon/amberalertcn/client/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hackthon.amberalertcn.client;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, LoginActivity.class));
             return true;
         }
 
