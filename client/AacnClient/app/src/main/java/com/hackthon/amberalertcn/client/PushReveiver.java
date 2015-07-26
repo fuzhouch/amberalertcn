7b07d0a162797a1ecc3d9dba88e712a60b0b70da
unknown <v-xuesli@RTCFUZHOUCHM1.redmond.corp.microsoft.com>
2015-07-26 13:44:07 +0800

    Add push components.

---
+++ client/AacnClient/app/src/main/java/com/hackthon/amberalertcn/client/PushReveiver.java
@@ -0,0 +1,55 @@
+package com.hackthon.amberalertcn.client;
+
+import android.content.Context;
+
+import com.baidu.android.pushservice.PushMessageReceiver;
+
+import java.util.List;
+
+/**
+ * Created by icyfox-bupt on 7/26/2015.
+ */
+
+public class PushReveiver extends PushMessageReceiver {
+
+    @Override
+    public void onBind(Context context, int errorCode, String appid,
+                       String userId, String channelId, String requestId) {
+        System.out.println("onbind\n" + errorCode + " " + userId + " " + channelId);
+    }
+
+    @Override
+    public void onUnbind(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onSetTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onDelTags(Context context, int i, List<String> list, List<String> list1, String s) {
+
+    }
+
+    @Override
+    public void onListTags(Context context, int i, List<String> list, String s) {
+
+    }
+
+    @Override
+    public void onMessage(Context context, String s, String s1) {
+
+    }
+
+    @Override
+    public void onNotificationClicked(Context context, String s, String s1, String s2) {
+
+    }
+
+    @Override
+    public void onNotificationArrived(Context context, String s, String s1, String s2) {
+
+    }
+}
