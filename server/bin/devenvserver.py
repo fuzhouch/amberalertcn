63b95d03b78fec70fb9cd7d6ac092cb8cd1fd675
Fuzhou Chen <fuzhouch@gmail.com>
2015-07-26 16:33:35 +0800

    Start server on specified port.

--- server/bin/devenvserver.py
+++ server/bin/devenvserver.py
@@ -13,4 +13,7 @@ if __name__ == '__main__':
     app = amberalertcn.Application(config='amberalertcn.config.devenv')
     api_page = amberalertcn.api.v1.api
     app.register_blueprint(api_page, url_prefix='/api/v1')
-    app.run(debug=True)
+    if len(sys.argv) != 1:
+        app.run(debug=True, host="0.0.0.0", port=int(sys.argv[1]))
+    else:
+        app.run(debug=True)
