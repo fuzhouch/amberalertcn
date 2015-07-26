fd18fff25e4dd9ac60e1dd2bd1c2e8d54764ee5e
Fuzhou Chen <fuzhouch@gmail.com>
2015-07-26 18:18:03 +0800

    Support /publishalert command from client. Also support push.

--- server/amberalertcn/api/v1/Pusher.py
+++ server/amberalertcn/api/v1/Pusher.py
@@ -11,43 +11,39 @@ sys.path.append("..")
 from Channel import *
 from Secret import *
 
-secret = Secret()
-apiKey = secret.apiKey
-secretKey = secret.secretKey
+message_key = "AmberAlertCn.Key1"
 
-message = "{'title':'baidu push','description':'message from python sdk'}"
-#message = json.dumps(message)
-message_key = "key1"
-#message_key = json.dumps(message_key)
-tagname = "test_tag"
+class Pusher(object):
+    def __init__(self, secret):
+        self.__secret = secret
 
-#user_list: list of (user_id, user_channel)
-#message: ["title", "description", "amber_alert_id", "brief"]
-def pushAlert_to_users(user_list, message):
-	for user in user_list:
-		channel = Channel(apiKey, secretKey)
-		push_type = 1
-		optional = dict()
-		optional[Channel.USER_ID] = user[0]
-		optional[Channel.CHANNEL_ID] = user[1]
-		optional[Channel.MESSAGE_TYPE] = 1
-		alertMessage = "{'title':'%s', 'description':'%s', 'custom_content': { 'amber_alert_id':'%s', 'from_user_id':'%s' }}" % (message[0], message[1], message[2], message[3])
-		ret = channel.pushMessage(push_type, alertMessage, message_key, optional)
-		print ret
+    def pushAlert_to_users(self, user_list, message):
+        for user in user_list:
+            channel = Channel(self.__secret.apiKey, self.__secret.secretKey)
+            push_type = 1
+            optional = dict()
+            optional[Channel.USER_ID] = user[0]
+            optional[Channel.CHANNEL_ID] = user[1]
+            optional[Channel.MESSAGE_TYPE] = 1
+            alertMessage = "{'title':'%s', 'description':'%s', 'custom_content': { 'amber_alert_id':'%s', 'from_user_id':'%s' }}" % (message[0], message[1], message[2], message[3])
+            #jsonMessage = json.dumps(alertMessage)
+            ret = channel.pushMessage(push_type, alertMessage, message_key, optional)
+            print(ret)
 
 #user: tuple of user, (user_id, channel_id)
 #message: ["title", "description", "amber_alert_id", "brief", "from_user_id"]
-def pushUpdate_to_users(user_list, message):
+    def pushUpdate_to_users(self, user_list, message):
 	for user in user_list:
-		channel = Channel(apiKey, secretKey)
-		push_type = 1
-		optional = dict()
-		optional[Channel.USER_ID] = user[0]
-		optional[Channel.CHANNEL_ID] = user[1]
-		optional[Channel.MESSAGE_TYPE] = 1
-		updateMessage = "{'title':'%s', 'description':'%s', 'custom_content': { 'amber_alert_id':'%s', 'from_user_id':'%s' }}" % (message[0], message[1], message[2], message[3])
-		ret = channel.pushMessage(push_type, updateMessage, message_key, optional)
-		print ret
+            channel = Channel(self.__secret.apiKey, self.__secret.secretKey)
+            push_type = 1
+            optional = dict()
+            optional[Channel.USER_ID] = user[0]
+            optional[Channel.CHANNEL_ID] = user[1]
+            optional[Channel.MESSAGE_TYPE] = 1
+            updateMessage = "{'title':'%s', 'description':'%s', 'custom_content': { 'amber_alert_id':'%s', 'from_user_id':'%s' }}" % (message[0], message[1], message[2], message[3])
+            #jsonMessage = json.dumps(updateMessage)
+            ret = channel.pushMessage(push_type, updateMessage, message_key, optional)
+            print ret
 
 
 ###
@@ -65,12 +61,13 @@ def test_pushUpdate_to_users():
 	message = ['UpdateTitle', 'UpdateDesc', 'amber_alert_id', 'from_user_id']
 	pushAlert_to_users(userlist, message)
 
-
 ###
 # Examples
 ###
+
+"""
 def test_pushMessage_to_user():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	push_type = 1
 	optional = dict()
 	optional[Channel.USER_ID] = user_id
@@ -81,7 +78,7 @@ def test_pushMessage_to_user():
 	print ret
 
 def test_pushMessage_to_tag():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	push_type = 2
 	tag_name = 'push'
 	optional = dict()
@@ -90,7 +87,7 @@ def test_pushMessage_to_tag():
 	print ret
 
 def test_pushMessage_to_all():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	push_type = 3
 	optional = dict()
 	ret = c.pushMessage(push_type, message, message_key, optional)
@@ -98,62 +95,61 @@ def test_pushMessage_to_all():
 
 
 def test_queryBindList():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	optional = dict()
 	optional[Channel.CHANNEL_ID] =  channel_id
 	ret = c.queryBindList(user_id, optional)	
 	print ret
 
 def test_verifyBind():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	optional = dict()
 	optional[Channel.DEVICE_TYPE] = 3;
 	ret = c.verifyBind(user_id, optional)
 	print ret
 
 def test_fetchMessage():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	ret = c.fetchMessage(user_id)
 	print ret	
 
 def test_deleteMessage():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	msg_id = "111"
 	ret = c.deleteMessage(user_id, msg_id)
 	print ret
 
 def test_setTag():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	optional = dict()
 	optional[Channel.USER_ID] = user_id
 	ret = c.setTag(tagname, optional)
 	print ret
 
 def test_fetchTag():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	ret = c.fetchTag()
 	print ret
 
 def test_deleteTag():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	optional = dict()
 	optional[Channel.USER_ID] = user_id
 	ret = c.deleteTag(tagname, optional)
 	print ret
 
 def test_queryUserTag():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	ret = c.queryUserTag(user_id)
 	print ret
 
 def test_queryDeviceType():
-	c = Channel(apiKey, secretKey)
+	c = Channel(self.__secret.apiKey, self.__secret.secretKey)
 	ret = c.queryDeviceType(channel_id)
 	print ret
 
 #test_pushMessage_to_user()
-"""
-if(__name__ == '__main__'):
+if __name__ == '__main__':
 	test_pushMessage_to_user()
 	time.sleep(1)
 	test_pushMessage_to_tag()
--- server/amberalertcn/core.py
+++ server/amberalertcn/core.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import amberalertcn.utils as utils
+import amberalertcn.api.v1.Pusher as pusher
+import flask
 
 class Core(object):
     def __init__(self, dbaccess, lbsaccess):
@@ -10,12 +12,12 @@ class Core(object):
 
     def update_location(self, user_id, channel_id, longitude, latitude):
         dbaccess = self.__dbaccess
-        amber_device_id = dbaccess.query_device_id_from_baidu_push(\
+        amber_device_id = dbaccess.query_device_id_from_baidu(\
                 user_id, channel_id)
         if amber_device_id is None:
             # It is a new device ID, but can it be a new user ID
             print("Create new device ID")
-            amber_user_id = dbaccess.query_amber_user_id_from_baidu(\
+            amber_user_id = dbaccess.query_user_id_from_baidu(\
                     user_id, channel_id)
             if amber_user_id is None:
                 amber_user_id = dbaccess.create_amber_user_id()
@@ -37,10 +39,43 @@ class Core(object):
                 }
         return resp
 
-    def publish_alsert(self, user_id, channel_id, \
+    def publish_alert(self, user_id, channel_id, \
             child_id, longitude, latitude):
-        return utils.make_json_response({\
-                "status_code": utils.httplib.OK })
+        dbaccess = self.__dbaccess
+        amber_from_user_id = dbaccess.query_user_id_from_baidu(\
+                user_id, channel_id)
+        amber_device_id = dbaccess.query_device_id_from_baidu(\
+                user_id, channel_id)
+        assert amber_device_id is not None
+        assert amber_from_user_id is not None
+
+        distance = 50 # TO BE DETERMINED
+
+        amber_alert_id = dbaccess.create_alert(amber_from_user_id, \
+                amber_device_id)
+        matched_device_list = dbaccess.query_device_list_in_range(\
+                amber_device_id, distance, longitude, latitude)
+        if len(matched_device_list) == 0:
+            return {\
+                    "status_code": utils.httplib.OK,\
+                    "alerted_count": 0}
+
+        message = [
+                "警告：有孩子走失！", # title
+                "三岁，有点胖，大红帽子绿上衣，包包头", # description
+                "三岁，有点胖，大红帽子绿上衣，包包头", # description
+                amber_alert_id, # amber_alert_id
+                amber_from_user_id # from_user_id
+                ]
+        print("Send")
+        p = pusher.Pusher(flask.current_app.config["AACN_SECRET"])
+        result_json = p.pushAlert_to_users(matched_device_list, message)
+        print("Done")
+        # TBD result_json will be handled later.
+
+        return { "status_code" : utils.httplib.OK,\
+                "alerted_count" : len(matched_device_list)\
+                }
 
     def send_message(self, sender_id, amber_alert_id, message):
         return utils.make_json_response({\
